f37bb53ff2d9917a0d76abc1b4715189d717c6cb
Miguel Angel <mp.ruesga@outlook.com>
2024-05-07 16:05:34 -0600

    dice roll app created

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:custom_app_test/gradient_container.dart';
+
+var colores = [Color.fromARGB(255, 1, 57, 102), Color.fromARGB(255, 1, 114, 206)];
+void main() {
+  runApp(
+    const MaterialApp(
+    home: Scaffold(
+      body: GradientContainer(
+          [Color.fromARGB(255, 1, 57, 102), Color.fromARGB(255, 1, 114, 206)]),
+    ),
+  ));
+}
