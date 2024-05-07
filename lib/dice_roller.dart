d4436ad24645ef0283826422e5eb4126cc8ff484
Miguel Angel <mp.ruesga@outlook.com>
2024-05-07 16:24:45 -0600

    added variables for second column

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -19,11 +19,17 @@ class _DiceRollerState extends State<DiceRoller> {
   var currentDiceRoll = 2;
   var activeDiceImage2 = 'assets/images/dice-2.png';
   var currentDiceRoll2 = 2;
+  var activeDiceImage3 = 'assets/images/dice-2.png';
+  var currentDiceRoll3 = 2;
+  var activeDiceImage4 = 'assets/images/dice-2.png';
+  var currentDiceRoll4 = 2;
 
   void rollDice() {
     setState(() {
       currentDiceRoll = randomizer.nextInt(6) + 1; // Value between 1 and 6
       currentDiceRoll2 = randomizer.nextInt(6) + 1; // Value between 1 and 6
+      currentDiceRoll3 = randomizer.nextInt(6) + 1; // Value between 1 and 6
+      currentDiceRoll4 = randomizer.nextInt(6) + 1; // Value between 1 and 6
     });
     // print('Changing image...');
   }
@@ -63,12 +69,12 @@ class _DiceRollerState extends State<DiceRoller> {
               Column(mainAxisSize: MainAxisSize.min,
                 children: [
                   Image.asset(
-                    'assets/images/dice-$currentDiceRoll.png',
+                    'assets/images/dice-$currentDiceRoll3.png',
                     width: 200,
                   ),
                 
                   Image.asset(
-                    'assets/images/dice-$currentDiceRoll2.png',
+                    'assets/images/dice-$currentDiceRoll4.png',
                     width: 200,
                   ),
                   const SizedBox(height: 20),
